df6a6bf6ef48f60c911a8715d43c4806fb58982a
Kamil <kamil.p.kubiak@gmail.com>
2022-05-12 21:21:31 +0200

    First commit, create django files

---
+++ pips.py
@@ -0,0 +1,9 @@
+#pip install virtualenv
+#pip install --user virtualenv
+#python -m virtualenv venv
+#Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+#venv/Scripts/Activate.ps1  
+#deavtivate
+#pip install django
+#django-admi
+#python manage.py runserver
