1fd56462614f439fb91e8c76107f48dd710d0538
Kamil <kamil.p.kubiak@gmail.com>
2022-05-18 21:54:52 +0200

    Create database and migrations

--- stocktracker/base/models.py
+++ stocktracker/base/models.py
@@ -1,3 +1,12 @@
+from turtle import update
 from django.db import models
 
-# Create your models here.
+class Assets(models.Model):
+    asset_name = models.TextField(max_length=30, null=False)
+    shortcut = models.TextField(max_length=10, null=False)
+    description = models.TextField(max_length=200, null=False)
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.asset_name
