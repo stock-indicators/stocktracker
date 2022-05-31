3b9652606ff50036cc6e5dd1c8c2c806c5a5dba9
Kamil <kamil.p.kubiak@gmail.com>
2022-05-31 21:36:17 +0200

    Create search bar

--- stocktracker/base/urls.py
+++ stocktracker/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('search/', views.search_address, name="search"),
     path('analyzer/', views.analyzer, name="analyzer")
 ]
--- stocktracker/base/views.py
+++ stocktracker/base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.cache import cache
+from .models import *
 
 
 
@@ -7,3 +10,14 @@ def home(request):
 
 def analyzer(request):
     return render(request, 'base/analyzer.html')
+
+def search_address(request):
+    asset_name = request.GET.get('asset_name')
+    asset_list = []
+    if asset_name:
+        assets_obj = Assets.objects.filter(asset_name__icontains=asset_name)
+
+        for assets_obj in assets_obj:
+            asset_list.append(assets_obj.asset_name)
+
+    return JsonResponse({'status' :200, 'data' : asset_list})
