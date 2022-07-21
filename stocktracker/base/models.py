fcaabdab570be02ab5d5582f7c21ec0c4179cf5d
Kamil <kamil.p.kubiak@gmail.com>
2022-07-21 21:16:08 +0200

    Segregate added assets

--- stocktracker/base/models.py
+++ stocktracker/base/models.py
@@ -17,9 +17,14 @@ class Assets(models.Model):
     description = models.TextField(max_length=200, null=False)
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-update', '-created']
+
     def __str__(self):
         return self.name
 
+ 
 
 class TwitterFollows(models.Model):
     name = models.TextField(max_length=50, null=False)
